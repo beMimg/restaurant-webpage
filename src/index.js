632938c68e9fce8cc6bcd08cb819a9e0f1ec5792
bernyig <bernardommguerreiro@gmail.com>
2023-08-30 16:38:47 +0200

    scroll functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const navbar = document.querySelector(".nav");
 const links = document.querySelector(".links");
 const date = document.querySelector(".date");
 const topLink = document.querySelector(".top-link");
+const allLinks = document.querySelectorAll(".scroll-link");
 //
 // set the current year on the date class
 date.textContent = new Date().getFullYear();
@@ -19,11 +20,11 @@ headlineBtn.addEventListener("click", function () {
   }
 });
 //
-
 // lets get the heigth of the nav bar and then when we scroll thro that specif height we implement fixed nav;
 window.addEventListener("scroll", function () {
   let currentHeight = pageYOffset;
   let navbarHeight = navbar.getBoundingClientRect().height;
+
   if (currentHeight > navbarHeight) {
     navbar.classList.add("fixed-nav");
   } else {
@@ -34,5 +35,26 @@ window.addEventListener("scroll", function () {
   } else {
     topLink.classList.remove("show-top-link");
   }
-  console.log(currentHeight);
+  // console.log(currentHeight);
+
+  allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      let id = e.currentTarget.getAttribute("href").slice(1);
+      let element = document.getElementById(id);
+      let elementDistance = element.offsetTop - navbarHeight;
+      let fixedNav = navbar.classList.contains("fixed-nav");
+      let linksContainerHeight = linksContainer.getBoundingClientRect().height;
+
+      if (!fixedNav) {
+        elementDistance = elementDistance - navbarHeight;
+      }
+
+      window.scrollTo({
+        left: 0,
+        top: elementDistance,
+      });
+      e.preventDefault();
+      linksContainer.style.height = 0;
+    });
+  });
 });
